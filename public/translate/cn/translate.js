ebad4e1d1b74cee204cb57977f17f3d64ff45393
Yang Lin <todoubaba@gmail.com>
2016-11-25 01:15:27 +0800

    Fixes translate.js

--- public/translate/cn/translate.js
+++ public/translate/cn/translate.js
@@ -22,6 +22,7 @@ var sourceVisible = localStorage.getItem('source-visible') === 'true';
    * @param container
    */
   function processContainer(container) {
+    var count = 0;
     for (var i = 0; i < container.children.length; i++) {
       var node = container.children[i];
 
@@ -40,35 +41,43 @@ var sourceVisible = localStorage.getItem('source-visible') === 'true';
         case 'H5':
         case 'H6':
         case 'HEADER':
+          count++;
           if (processBlock(node)) {
             i++;
-          }
-          break;
-        case 'LI':
-          if($(node).find('p').length <= 1){
-            if (processBlock(node)) {
-              i++;
-            }
+            count++;
           }
           break;
         case 'TD':
         case 'TH':
+        case 'UL':
+        case 'OL':
           processContainer(node);
-          // return; // stop
           break;
         default:
+          if (node.innerText === 'Back to top') {
+            var x = 0;
+          }
           if (node.children.length > 0) {
-            processContainer(node);
             // For <li><p>...</p></li>, processes it as block.
-            // if (node.children.length === 1) {
-            //   if (processBlock(node)) {
-            //     i++;
-            //   }
-            // }
+            if (processContainer(node) <= 1) {
+              if (processBlock(node)) {
+                i++;
+              }
+            } else {
+              count++;
+            }
+          } else {
+            if (node.tagName === 'A' && node.classList.contains('to-top')) {
+              if (processBlock(node)) {
+                i++;
+              }
+            }
           }
           break;
       }
     }
+
+    return count;
   }
 
   /**
@@ -83,13 +92,19 @@ var sourceVisible = localStorage.getItem('source-visible') === 'true';
     var $sibling = $(sibling);
 
     if (sibling) {
+      if (sibling.tagName === 'LI') {
+        processBlock(sibling);
+      }
+
       if (isClonedNode(current, sibling)) {
         if (isPureEnglish(current.textContent)) {
           $current.addClass('original-english');
           $sibling.addClass('translated');
           $sibling.addClass('translated-cn');
           $sibling.after($current);
-          $sibling.on('click', function () {
+          $sibling.on('click', function (event) {
+            // for nested structure.
+            event.stopPropagation();
             $current.toggleClass('hidden');
           });
           return true;
