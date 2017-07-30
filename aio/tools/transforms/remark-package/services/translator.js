f7b656443df328947acd0ce09412735f67c74eb3
Zhicheng Wang <asnowwolf@gmail.com>
2017-07-30 16:23:26 +0800

    fix: limit tagName to whole world

--- aio/tools/transforms/remark-package/services/translator.js
+++ aio/tools/transforms/remark-package/services/translator.js
@@ -1,5 +1,5 @@
 function mark(text) {
-  return text.replace(/<(h\d|header|p|a)([\s\S]*?)>([\s\S]+?)<\/\1>/gi, function ($0, $1, $2, $3) {
+  return text.replace(/<(h\d|header|p|a)(\b[\s\S]*?)>([\s\S]+?)<\/\1>/gi, function ($0, $1, $2, $3) {
     const suffix = isTranslation($3) ? 'result' : 'origin=off';
     return `<${$1} translation-${suffix} ${$2}>${$3}</${$1}>`;
   });
