104edf62f0359f52c521a92b43d517500f938726
Zhicheng Wang <zcwang@thoughtworks.com>
2017-08-06 13:49:04 +0800

    fix: minor improvement

--- aio/tools/transforms/remark-package/services/translator.spec.js
+++ aio/tools/transforms/remark-package/services/translator.spec.js
@@ -1,9 +1,10 @@
-// TODO: replace mocha with jasmine
 const { expect } = require('chai');
 
-const { mark} = require('./translator');
+const { mark } = require('./translator');
 
-it('mark translation results and origins', () => {
-  expect(mark('<h1>AngularJS to Angular</h1>\n<h1>从 AngularJS 到 Angular 快速参考</h1>'))
-    .equal('<h1 translation-origin=off >AngularJS to Angular</h1>\n<h1 translation-result >从 AngularJS 到 Angular 快速参考</h1>');
+describe('translator', () => {
+  it('mark translation results and origins', () => {
+    expect(mark('<h1>AngularJS to Angular</h1>\n<h1>从 AngularJS 到 Angular 快速参考</h1>'))
+      .toEqual('<h1 translation-origin=off >AngularJS to Angular</h1>\n<h1 translation-result >从 AngularJS 到 Angular 快速参考</h1>');
+  });
 });
