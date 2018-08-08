b8dbc5d16ea7400425100dd1ce9e88ec4ab9978d
Zhicheng Wang <zcwang@thoughtworks.com>
2018-08-08 11:18:52 +0800

    chore: add auto-publish script

---
+++ aio/publish.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -x
+
+cd `dirname $0`
+
+yarn build
+rm -fr ../../ng-docs.github.io/*
+cp -r dist/* ../../ng-docs.github.io
+cd ../../ng-docs.github.io
+cp index.html 404.html
+echo v6.angular.live > CNAME
+git add .
+git commit -am "publish"
+
+git push
+
+cd -
