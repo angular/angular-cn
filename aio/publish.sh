d8d60257f5a3162c5f2e16c624a2f47a87b23a7a
Zhicheng Wang <zcwang@thoughtworks.com>
2018-09-16 06:43:43 +0800

    chore: 不再移除原有文件，以保持升级时的兼容性

--- aio/publish.sh
+++ aio/publish.sh
@@ -9,13 +9,10 @@ set -x
 cd `dirname $0`
 
 yarn build
-rm -fr ../../ng-docs.github.io/*
-cp -r dist/* ../../ng-docs.github.io
+cp -r dist/* ../../ng-docs.github.io/
 cd ../../ng-docs.github.io
+
 cp index.html 404.html
-echo v6.angular.live > CNAME
-echo angular.cn >> CNAME
-echo www.angular.cn >> CNAME
 git add .
 git commit -am "publish"
 
