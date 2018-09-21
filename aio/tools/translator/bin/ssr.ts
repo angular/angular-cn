cf3c92e3178162721010cdaf0a63d3f9d1e6c645
Zhicheng Wang <zcwang@thoughtworks.com>
2018-09-21 14:36:58 +0800

    docs: 支持 API 列表的 SEO

--- aio/tools/translator/bin/ssr.ts
+++ aio/tools/translator/bin/ssr.ts
@@ -1,33 +1,55 @@
 import * as fs from 'fs';
 import * as mkdirp from 'mkdirp';
 import * as path from 'path';
+import * as klawSync from 'klaw-sync';
 
-function buildInnerPages(): void {
+const rootElementPattern = /<aio-shell\b[\s\S]*<\/aio-shell>/;
+
+const indexTemplate = fs.readFileSync('./dist/index.html', 'utf-8');
+const aioShellTemplate = fs.readFileSync(__dirname + '/../assets/aio-shell-template.html');
+const pageTemplate = indexTemplate.replace(rootElementPattern, `${aioShellTemplate}`);
+
+function composePage(url) {
+  const { title, contents } = JSON.parse(fs.readFileSync(`./dist/generated/docs/${url}.json`, 'utf-8'));
+
+  if (!contents) {
+    return;
+  }
+  const ssrContent = contents.replace(/href="(?!http)(.{2,}?)"/gi, 'href="/$1"');
+  const pageContent = pageTemplate
+    .replace('<title>Angular Docs</title>', `<title>${title} - Angular 官方文档</title>`)
+    .replace('<aio-page-content-placeholder></aio-page-content-placeholder>', ssrContent);
+  mkdirp.sync(path.dirname(`./dist/${url}`));
+  fs.writeFileSync(`./dist/${url}.html`, pageContent, 'utf-8');
+}
+
+function buildGuidePages(): void {
   const navigation = fs.readFileSync('./content/navigation.json', 'utf-8');
-  const indexTemplate = fs.readFileSync('./dist/index.html', 'utf-8');
-  const aioShellTemplate = fs.readFileSync(__dirname + '/../assets/aio-shell-template.html');
-  const pageTemplate = indexTemplate.replace('<aio-shell></aio-shell>', `${aioShellTemplate}`);
-  const urls = navigation.match(/"url": "(.*?)"/g)
+  navigation.match(/"url": "(.*?)"/g)
     .map((entry) => entry.replace(/^"url": "(.*?)".*$/, '$1'))
     .filter(url => url.slice(0, 4) !== 'http')
-    .forEach(url => {
-      const generated: { title: string, contents: string } = JSON.parse(fs.readFileSync(`./dist/generated/docs/${url}.json`, 'utf-8'));
+    .forEach(url => composePage(url));
+}
 
-      const content = generated.contents.replace(/href="(?!http)(.{2,}?)"/gi, 'href="/$1"');
-      const pageContent = pageTemplate
-        .replace('<title>Angular Docs</title>', `<title>${generated.title} - Angular 官方文档</title>`)
-        .replace('<aio-page-content-placeholder></aio-page-content-placeholder>', content);
-      mkdirp.sync(path.dirname(`./dist/${url}`));
-      fs.writeFileSync(`./dist/${url}.html`, pageContent, 'utf-8');
-    });
+function buildApiPages(): void {
+  const apiList = klawSync('./dist/generated/docs/api', { nodir: true })
+    .map(file => file.path.replace(/^.*generated\/docs(\/.*).json$/, '$1'));
+  apiList.forEach(url => composePage(url));
+
+  const links = apiList.map(url => `<a href="${url}">${url}</a>`).join('\n');
+  const apiListContent = fs.readFileSync('./dist/api.html', 'utf-8')
+    .replace(rootElementPattern, `<aio-shell><h3>API List</h3>${links}</aio-shell>`);
+
+  fs.writeFileSync(`./dist/api.html`, apiListContent, 'utf-8');
 }
 
 function buildIndexPage(): void {
   const indexTemplate = fs.readFileSync('./dist/index.html', 'utf-8');
   const aioShellIndex = fs.readFileSync(__dirname + '/../assets/aio-shell-index.html');
-  const content = indexTemplate.replace('<aio-shell></aio-shell>', `${aioShellIndex}`);
+  const content = indexTemplate.replace(rootElementPattern, `${aioShellIndex}`);
   fs.writeFileSync(`./dist/index.html`, content, 'utf-8');
 }
 
-buildInnerPages();
+buildGuidePages();
+buildApiPages();
 buildIndexPage();
