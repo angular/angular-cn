34b81801836abb2ab85c5bb7750a9b969c7c1861
Zhicheng Wang <zcwang@thoughtworks.com>
2018-10-27 09:22:46 +0800

    fix: 修改翻译脚本

--- aio/tools/translator/bin/translate-one.ts
+++ aio/tools/translator/bin/translate-one.ts
@@ -1,7 +1,8 @@
 #!/usr/bin/env ts-node
 
 import { dirs } from '../dirs';
+import * as path from 'path';
 import { translateFile } from '../translate';
 
-const filename = 'guide/reactive-forms.md';
-translateFile(__dirname + '/../../../../../angular/aio/content/' + filename, dirs.content + filename);
+const filename = path.join(dirs.content, process.argv[2]);
+translateFile(filename, filename);
