4c9e1a6a3dd3c80d7671995f3be14f2406030d08
Zhicheng WANG <zcwang@thoughtworks.com>
2019-01-04 10:59:10 +0800

    fix: 为 Hn 添加 id，以便和英文锚点保持一致

--- aio/tools/transforms/remark-package/services/translator.js
+++ aio/tools/transforms/remark-package/services/translator.js
@@ -1,4 +1,4 @@
-const {markAndSwapAll} = require('@awesome-fe/translate');
+const {addIdForHeaders, markAndSwapAll} = require('@awesome-fe/translate');
 
 const {JSDOM} = require('jsdom');
 
@@ -7,6 +7,7 @@ const selectorGroups = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'header', 'section',
 function mark(text) {
   const dom = new JSDOM(text);
   const body = dom.window.document.body;
+  addIdForHeaders(body);
   markAndSwapAll(body, selectorGroups);
   return body.innerHTML;
 }
