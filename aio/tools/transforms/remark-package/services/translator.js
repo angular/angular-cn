5aa7fbfe6489c6cfc8491a604c5c5745c1d1f1b7
Zhicheng WANG <zcwang@thoughtworks.com>
2019-01-27 22:13:07 +0800

    fix: 不再处理 span 和 a

--- aio/tools/transforms/remark-package/services/translator.js
+++ aio/tools/transforms/remark-package/services/translator.js
@@ -2,7 +2,7 @@ const {addIdForHeaders, markAndSwapAll} = require('@awesome-fe/translate');
 
 const {JSDOM} = require('jsdom');
 
-const selectorGroups = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'header', 'section', 'p', 't', 'span', 'a'];
+const selectorGroups = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'header', 'section', 'p', 't'];
 
 function mark(text) {
   const dom = new JSDOM(text);
