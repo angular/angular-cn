3e0ef413a8d88f5f3786b7bf31b9ab5d60017d79
Zhicheng WANG <zcwang@thoughtworks.com>
2019-06-06 20:53:13 +0800

    fix: 错误的合并顺序

--- packages/core/src/metadata/di.ts
+++ packages/core/src/metadata/di.ts
@@ -331,14 +331,15 @@ export interface ViewChildDecorator {
    *   **selector** - 用于查询的指令类型或名字。
    *
    * * **read** - read a different token from the queried elements.
+   *
+   *   **read** - 从查询到的元素中读取另一个令牌。
+   *
    * * **static** - whether or not to resolve query results before change detection runs (i.e.
    * return static results only). If this option is not provided, the compiler will fall back
    * to its default behavior, which is to use query results to determine the timing of query
    * resolution. If any query results are inside a nested view (e.g. *ngIf), the query will be
    * resolved after change detection runs. Otherwise, it will be resolved before change detection
    * runs.
-   *
-   *   **read** - 从查询到的元素中读取另一个令牌。
    *
    * Supported selectors include:
    *
